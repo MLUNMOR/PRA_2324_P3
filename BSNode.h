ddc9610a8d8377e6687f1af41c5a01eebb76f32e
Marc <mlunmor@etsinf.upv.es>
2025-12-02 19:20:33 +0100

    BSNode

---
+++ BSNode.h
@@ -0,0 +1,25 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+        // miembros públicos
+        T elem;
+        BSNode<T> *left;
+        BSNode<T> *right;
+
+        //Método constructor
+        BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr);
+
+        //sobrecarga del operador << (imprimir el elemento por pantalla)
+        friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn){
+            out << "Valor del elemento: " << elem;
+
+            return out;
+        }
+};
+
+#endif
